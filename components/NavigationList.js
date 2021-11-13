3dfad57a97fa7d83e9278068fbaaecdb5b78f981
Natalia Vale Asari <natalia.vale.asari@gmail.com>
2021-11-13 18:09:46 -0300

    Changed English CV link.

--- components/NavigationList.js
+++ components/NavigationList.js
@@ -22,7 +22,7 @@ export default function NavigationList() {
 							  	<a href="https://ui.adsabs.harvard.edu/search/q=author%3A(%22vale%20asari%2C%20n.%22%20OR%20%22asari%2C%20n.%20v.%22)&sort=date%20desc%2C%20bibcode%20desc&p_=0">{translations.Publications}</a>
 							  </li>
 							  <li>
-							  	<a href={translations.CvUrl}>CV (Lattes)</a>
+							  	<a href={translations.CvUrl}>{translations.CvName}</a>
 							  </li>
 							  <li>
 							  	<a href="http://minerva.ufsc.br/~natalia/phd_thesis/Natalia2010_thesis_finalversion.pdf">{translations.Thesis}</a>
